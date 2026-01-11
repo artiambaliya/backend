60e558dc9da7cdf1c149d38b8a60992f04fee8be
artiambaliya <ambaliyaarti397@gmail.com>
2026-01-11 08:51:59 +0530

    add env file

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const app = express()
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL)
-.then(() => console.log("connected !"))
+.then(() => console.log("connected !"));
 
 app.use("/", allRoutes)
-app.use(errorHandler);
+app.use(errorHandler)
 
 app.listen(8000, () => console.log("server is running"));
